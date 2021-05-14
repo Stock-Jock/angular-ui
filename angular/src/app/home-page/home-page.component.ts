fe6aedbdd8d012b56dd73f566635a740d4a1ebd9
Daniel Hopson <hdhopson88@yahoo.com>
2021-05-14 10:34:34 -0700

    Updates home page component with angular router

--- angular/src/app/home-page/home-page.component.ts
+++ angular/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
