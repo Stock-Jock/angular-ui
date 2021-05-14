167ca7d9a86a29e75bbe52ce1949b0a6ef9e70f4
Daniel Hopson <hdhopson88@yahoo.com>
2021-05-14 16:59:59 -0700

    Retrieves stock data from crud

--- angular/src/app/sidebar/sidebar.component.ts
+++ angular/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
+import { Stock } from './../models/stock';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { StockServiceService } from '../services/stock-service/stock-service.service';
 
@@ -8,10 +10,23 @@ import { StockServiceService } from '../services/stock-service/stock-service.ser
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  
+  stockList: Stock[];
 
-  constructor(stockService: StockServiceService) { }
+  constructor(private stockService: StockServiceService, private router: Router) { 
+    this.stockList = [];
+  }
 
   ngOnInit(): void {
+    this.router.events.subscribe();
+    this.getStockList();
+  }
+
+  getStockList(): any {
+    this.stockService.getStockItems().subscribe((res) => {
+      this.stockList = res;
+      console.log(this.stockList);
+    })
   }
 
 }
